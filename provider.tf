e88a6241c810743c7f8b95861f7f9982741808d2
simkibo12 <74437731+simkibo12@users.noreply.github.com>
2022-10-04 15:34:23 +0900

    Create provider.tf

---
+++ provider.tf
@@ -0,0 +1,12 @@
+terraform {
+  required_providers {
+    ncloud = {
+      source = "NaverCloudPlatform/ncloud"
+      version = "2.3.0"
+    }
+  }
+}
+
+provider "ncloud" {
+  # Configuration options
+}
